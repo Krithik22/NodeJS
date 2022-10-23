1f1c7ff0ce59aaaa65126469254cd85dde181215
Krithik22 <krithiksaiprasad22@gmail.com>
2022-10-24 01:10:56 +0530

    app js file added

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('hello');
+console.log('hello');
+setTimeout(()=>{
+    console.log('after 2 seconds');
+},3000);
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
